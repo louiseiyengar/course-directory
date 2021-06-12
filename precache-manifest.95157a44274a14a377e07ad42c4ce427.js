fd8a04f3625aea11cded59ac4e8f78cb35fb425d
Louise Iyengar <lki@louiseiyengar.com>
2021-06-12 09:30:37 -0400

    Updates

---
+++ precache-manifest.95157a44274a14a377e07ad42c4ce427.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2766e23ada0cc9bf483e",
+    "url": "/course-directory/static/css/main.00baeb36.chunk.css"
+  },
+  {
+    "revision": "2766e23ada0cc9bf483e",
+    "url": "/course-directory/static/js/main.2766e23a.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "c87793d8a10a09137c3e43eedb4f3447",
+    "url": "/course-directory/index.html"
+  }
+];
